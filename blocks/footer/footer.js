addd696a0cab1322f9607846295d54e03141b57a
anagarwa <anagarwa@adobe.com>
2024-11-20 10:16:09 +0530

    fix: adding styling for header

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,6 +1,48 @@
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
+
+function addIconsToList(ulElement) {
+  // Select all li elements within the given ul
+  const listItems = ulElement.querySelectorAll('li');
+
+  listItems.forEach(li => {
+    // Find the <a> tag within each <li>
+    const link = li.querySelector('a');
+    if (link && link.title) {
+      link.textContent = '';
+
+      if (link.title.toLowerCase() === 'twitter') {
+        // For Twitter, add an <img> element
+        const imgElement = document.createElement('img');
+        imgElement.src = '../../icons/x-white-logo.png';
+        imgElement.alt = '';
+        imgElement.className = 'img-twitter';
+
+        // Append the <img> element to the <a> tag
+        link.append(imgElement);
+      } else {
+        const iconElement = document.createElement('i');
+        iconElement.className = `icon-${link.title.toLowerCase()}`;
+        link.append(iconElement);
+      }
+
+      // Create a new <i> element with the desired class
+
+    }
+  });
+}
+
+function addCategoryClass(leftMenu) {
+  // Select all first-level <li> elements within the .default-content-wrapper
+  const topLevelItems = leftMenu.querySelectorAll('.default-content-wrapper > ul > li');
+
+  topLevelItems.forEach(li => {
+    // Add the 'category' class to each first-level <li> element
+    li.classList.add('category');
+  });
+}
+
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
@@ -16,5 +58,52 @@ export default async function decorate(block) {
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
+  const leftMenu = footer.querySelector('.left-menu');
+
+  addCategoryClass(leftMenu);
+
+  const rightMenu = footer.querySelector('.right-menu');
+
+  const socialMediaLink = rightMenu.querySelector('.social-media ul');
+  addIconsToList(socialMediaLink);
+
+  const appStoreLink = rightMenu.querySelector('.app-store ul');
+  const listItems = appStoreLink.querySelectorAll('li');
+
+  listItems.forEach(li => {
+    // Find the <a> tag within each <li>
+    const link = li.querySelector('a');
+    if (link && link.title) {
+      link.textContent = '';
+
+      if (link.title.toLowerCase().includes('apple')) {
+        // For Twitter, add an <img> element
+        const imgElement = document.createElement('img');
+        imgElement.src = '../../icons/apple_app_store.svg';
+        imgElement.alt = '';
+        imgElement.className = 'img-app img-apple';
+
+        // Append the <img> element to the <a> tag
+        link.append(imgElement);
+      } else if (link.title.toLowerCase().includes('google')) {
+        // For Twitter, add an <img> element
+        const imgElement = document.createElement('img');
+        imgElement.src = '../../icons/google_play_store.svg';
+        imgElement.alt = '';
+        imgElement.className = 'img-app img-google';
+
+        // Append the <img> element to the <a> tag
+        link.append(imgElement);
+      }
+
+      // Create a new <i> element with the desired class
+
+    }
+  });
+
+
+
   block.append(footer);
+
+
 }
